513cae14cba3a4f50eb435dbd271fa317228b984
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2024-03-31 15:37:16 +0000

    form example

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, Attribute, div, input, text)
+import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
@@ -10,6 +10,7 @@ import Html.Events exposing (onInput)
 -- MAIN
 
 
+main : Program () Model Msg
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
@@ -19,13 +20,15 @@ main =
 
 
 type alias Model =
-  { content : String
+  { name : String
+  , password : String
+  , passwordAgain : String
   }
 
 
 init : Model
 init =
-  { content = "" }
+  Model "" "" ""
 
 
 
@@ -33,14 +36,22 @@ init =
 
 
 type Msg
-  = Change String
+  = Name String
+  | Password String
+  | PasswordAgain String
 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Change newContent ->
-      { model | content = newContent }
+    Name name ->
+      { model | name = name }
+
+    Password password ->
+      { model | password = password }
+
+    PasswordAgain password ->
+      { model | passwordAgain = password }
 
 
 
@@ -50,7 +61,21 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-    [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
-    , div [] [ text (String.reverse model.content) ]
-    , div [] [ text (String.fromInt (String.length model.content)) ]
+    [ viewInput "text" "Name" model.name Name
+    , viewInput "password" "Password" model.password Password
+    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+    , viewValidation model
     ]
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+  input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+  if model.password == model.passwordAgain then
+    div [ style "color" "green" ] [ text "OK" ]
+  else
+    div [ style "color" "red" ] [ text "Passwords do not match!" ]
