39fa55d5356d0f514afcc0014084cdc70152cc52
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2024-04-01 01:46:21 +0000

    add password validators

--- src/Main.elm
+++ src/Main.elm
@@ -76,6 +76,75 @@ viewInput t p v toMsg =
 viewValidation : Model -> Html msg
 viewValidation model =
   if model.password == model.passwordAgain then
-    div [ style "color" "green" ] [ text "OK" ]
+    validatePassword model.password
   else
     div [ style "color" "red" ] [ text "Passwords do not match!" ]
+
+validatePassword : String -> Html msg
+validatePassword pwd =
+  let
+    validation = combineValidators
+      [
+        upperValidator,
+        lowerValidator,
+        digitValidator,
+        lengthValidator
+      ] pwd
+  in
+    case validation of
+      Nothing -> div [ style "color" "green" ] [ text "OK" ]
+      Just v -> 
+        let
+          liError t = Html.li [] [text t]
+        in 
+          div [ style "color" "red" ] [
+            text "Failed Validators:",
+            ul [] (List.map liError v)
+          ]
+
+combineValidators : List (a -> Maybe b) -> a -> Maybe (List b)
+combineValidators v s =
+  combine (List.map (flip s) v)
+
+combine: List (Maybe a) -> Maybe (List a)
+combine l = 
+  let
+    filtered = List.filterMap identity l
+  in 
+    case filtered of
+      [] -> Nothing
+      _ -> Just filtered
+
+upperValidator : String -> Maybe String
+upperValidator = (validator hasUpper "Passwords must have Upper lcase letters")
+
+lowerValidator : String -> Maybe String
+lowerValidator =(validator hasLower "Passwords must have Lower case letter")
+
+digitValidator : String -> Maybe String
+digitValidator = (validator hasDigit "Passwords must have digits")
+
+lengthValidator : String -> Maybe String
+lengthValidator = (validator isLongEnough "Passwords must be longer than 8 characters")
+
+flip : a -> (a -> b) -> b
+flip s f = f s
+
+validator : (a -> Bool) -> b -> a -> Maybe b
+validator v e s =
+  if v s then
+    Maybe.Nothing
+  else
+    Maybe.Just e
+
+hasUpper : String -> Bool
+hasUpper = String.any Char.isUpper
+
+hasLower : String -> Bool
+hasLower = String.any Char.isLower
+
+hasDigit : String -> Bool
+hasDigit = String.any Char.isDigit
+
+isLongEnough : String -> Bool
+isLongEnough s = String.length s >= 8
