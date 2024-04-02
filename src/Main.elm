18dd67792145f30c5d3a638ec57cde8927eb7287
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2024-04-02 16:18:51 +0000

    add svg die

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,8 @@ import Browser
 import Html exposing (..)
 import Html.Events exposing (..)
 import Random
+import Svg as S exposing (circle, rect, svg)
+import Svg.Attributes as SA exposing (..)
 
 
 
@@ -69,6 +71,7 @@ update msg model =
             )
 
 
+
 -- SUBSCRIPTIONS
 
 
@@ -83,9 +86,69 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
+    let
+        circles: Int -> List (Int, Int)
+        circles dieFace =
+            List.concat [ 
+              if 0 == modBy 2 dieFace then [] else [( 60, 60 )]
+              ,if dieFace > 1 then [( 90, 30 ), (30, 90)] else []
+              ,if dieFace > 3 then [( 30, 30 ), (90,90)] else []
+              ,if dieFace > 5 then [( 30, 60 ), (90, 60)] else []
+            ]
+
+        buildCircles ( x, y ) =
+            S.circle
+                [ SA.cx (String.fromInt x)
+                , SA.cy (String.fromInt y)
+                , SA.r "10"
+                , SA.stroke "white"
+                , SA.fill "white"
+                ]
+                []
+    in
     div []
         [ 
-          div [] [ text (String.fromInt model.dieFace1) ]
-        , div [] [ text (String.fromInt model.dieFace2) ]
+        S.svg
+            [ SA.width "120"
+            , SA.height "120"
+            , SA.viewBox "0 0 120 120"
+            ]
+            (List.append
+                [ S.rect
+                    [ SA.x "10"
+                    , SA.y "10"
+                    , SA.width "100"
+                    , SA.height "100"
+                    , SA.rx "15"
+                    , SA.ry "15"
+                    ]
+                    []
+                ]
+                (List.map
+                    buildCircles
+                    (circles model.dieFace1)
+                )
+            )
+        , S.svg
+            [ SA.width "120"
+            , SA.height "120"
+            , SA.viewBox "0 0 120 120"
+            ]
+            (List.append
+                [ S.rect
+                    [ SA.x "10"
+                    , SA.y "10"
+                    , SA.width "100"
+                    , SA.height "100"
+                    , SA.rx "15"
+                    , SA.ry "15"
+                    ]
+                    []
+                ]
+                (List.map
+                    buildCircles
+                    (circles model.dieFace2)
+                )
+            )
         , button [ onClick Roll ] [ text "Roll" ]
         ]
