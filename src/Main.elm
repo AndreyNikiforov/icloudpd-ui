3b90fc341fd3e18691105e7eadc056dd6dff3f92
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2024-04-13 17:42:09 +0000

    do connect-disconnect ping pong

--- src/Main.elm
+++ src/Main.elm
@@ -3,16 +3,18 @@ module Main exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Events exposing (..)
-import Svg as S
-import Svg.Attributes as SA exposing (fillOpacity)
+import List exposing (foldl)
+import Random exposing (initialSeed)
 import Task
 import Time
+import Tuple
 
 
 
 -- MAIN
 
 
+main : Program () Model Msg
 main =
     Browser.element
         { init = init
@@ -26,17 +28,25 @@ main =
 -- MODEL
 
 
+type alias Expiration =
+    Time.Posix
+
+
+type Connection
+    = Connected Expiration
+    | Disconnected Expiration
+
+
 type alias Model =
-    { zone : Time.Zone
-    , time : Time.Posix
-    , paused : Bool
+    { time : Time.Posix
+    , connection : Connection
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model Time.utc (Time.millisToPosix 0) False
-    , Task.perform AdjustTimeZone Time.here
+    ( Model (Time.millisToPosix 0) (Disconnected (Time.millisToPosix 0))
+    , generateDisconnect
     )
 
 
@@ -46,9 +56,71 @@ init _ =
 
 type Msg
     = Tick Time.Posix
-    | AdjustTimeZone Time.Zone
-    | Pause
-    | Resume
+      -- connection
+    | Disconnect Expiration
+    | Connect Expiration
+    | StartDisconnect
+    | StartConnect
+
+
+timeToRandom : Random.Generator a -> Time.Posix -> a
+timeToRandom g =
+    Tuple.first << Random.step g << Random.initialSeed << Time.posixToMillis
+
+
+disconnectGenerator : Random.Generator Int
+disconnectGenerator =
+    Random.int 5000 10000
+
+
+connectGenerator : Random.Generator Int
+connectGenerator =
+    Random.int 10000 20000
+
+
+generateExpiration : Random.Generator Int -> Time.Posix -> Time.Posix
+generateExpiration g t =
+    let
+        offset =
+            timeToRandom g t
+    in
+    (Time.millisToPosix << (+) offset << Time.posixToMillis) t
+
+
+generateDisconnect : Cmd Msg
+generateDisconnect =
+    let
+        cmd =
+            Disconnect << generateExpiration disconnectGenerator
+    in
+    Task.perform cmd Time.now
+
+
+generateConnect : Cmd Msg
+generateConnect =
+    let
+        cmd =
+            Connect << generateExpiration connectGenerator
+    in
+    Task.perform cmd Time.now
+
+
+expireConnection : Connection -> Time.Posix -> Cmd Msg
+expireConnection connection newTime =
+    case connection of
+        Disconnected exp ->
+            if Time.posixToMillis exp < Time.posixToMillis newTime then
+                generateConnect
+
+            else
+                Cmd.none
+
+        Connected exp ->
+            if Time.posixToMillis exp < Time.posixToMillis newTime then
+                generateDisconnect
+
+            else
+                Cmd.none
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -56,23 +128,20 @@ update msg model =
     case msg of
         Tick newTime ->
             ( { model | time = newTime }
-            , Cmd.none
+            , expireConnection model.connection newTime
             )
 
-        AdjustTimeZone newZone ->
-            ( { model | zone = newZone }
-            , Cmd.none
-            )
+        Disconnect exp ->
+            ( { model | connection = Disconnected exp }, Cmd.none )
 
-        Pause ->
-            ( { model | paused = True }
-            , Cmd.none
-            )
+        Connect exp ->
+            ( { model | connection = Connected exp }, Cmd.none )
 
-        Resume ->
-            ( { model | paused = False }
-            , Cmd.none
-            )
+        StartConnect ->
+            ( model, generateConnect )
+
+        StartDisconnect ->
+            ( model, generateDisconnect )
 
 
 
@@ -80,137 +149,89 @@ update msg model =
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
-    if model.paused then
-        Sub.none
-
-    else
-        Time.every 1000 Tick
+subscriptions _ =
+    Time.every 1000 Tick
 
 
 
 -- VIEW
 
 
-leftPadded : Int -> Int -> String
-leftPadded k n =
-    String.right k (String.fromInt (10 ^ k + n))
+timeLeft : Time.Posix -> Time.Posix -> Maybe ( Int, String )
+timeLeft s e =
+    let
+        em =
+            Time.posixToMillis e
+
+        sm =
+            Time.posixToMillis s
+
+        diffm =
+            em - sm
+
+        multipliers : List ( Int, String )
+        multipliers =
+            [ ( 1000 * 60 * 60 * 24 * 365, "year(s)" )
+            , ( 1000 * 60 * 60 * 24 * 30, "month(s)" )
+            , ( 1000 * 60 * 60 * 24, "day(s)" )
+            , ( 1000 * 60 * 60, "hour(s)" )
+            , ( 1000 * 60, "minute(s)" )
+            , ( 1000, "second(s)" )
+            ]
+
+        div : Int -> ( Int, a ) -> ( Int, a )
+        div d0 ( f0, s0 ) =
+            ( d0 // f0, s0 )
+
+        bucketed =
+            List.map (div diffm) multipliers
+
+        gtZero ( a, _ ) =
+            a > 0
+
+        maxBucket =
+            bucketed
+                |> List.filter gtZero
+                |> List.head
+    in
+    maxBucket
 
 
 view : Model -> Html Msg
 view model =
     let
-        hour =
-            Time.toHour model.zone model.time
+        expirationText : Maybe ( Int, String ) -> String
+        expirationText tl =
+            case tl of
+                Just ( t, m ) ->
+                    "Expires in " ++ String.fromInt t ++ " " ++ m
 
-        hourView =
-            leftPadded 2 hour
+                Nothing ->
+                    "Expired"
 
-        minuteView =
-            leftPadded 2 minute
-
-        minute =
-            Time.toMinute model.zone model.time
-
-        secondView =
-            leftPadded 2 second
-
-        second =
-            Time.toSecond model.zone model.time
-
-        handDegree t c =
-            let
-                totalF =
-                    toFloat t
-
-                currentF =
-                    toFloat c
-
-                ratio =
-                    currentF / totalF
-            in
-            round (360 * ratio - 180)
-
-        hourHandView h =
-            S.rect
-                [ SA.x "57"
-                , SA.y "57"
-                , SA.width "6"
-                , SA.height "30"
-                , SA.rx "2"
-                , SA.ry "2"
-                , SA.stroke "black"
-                , SA.fill "black"
-                , SA.fillOpacity "25%"
-                , SA.transform ("rotate(" ++ String.fromInt (handDegree 12 h) ++ " 60 60)")
-                ]
-                []
-
-        minuteHandView h =
-            S.rect
-                [ SA.x "58"
-                , SA.y "58"
-                , SA.width "4"
-                , SA.height "50"
-                , SA.rx "2"
-                , SA.ry "2"
-                , SA.stroke "black"
-                , SA.fill "black"
-                , SA.fillOpacity "25%"
-                , SA.transform ("rotate(" ++ String.fromInt (handDegree 60 h) ++ " 60 60)")
-                ]
-                []
-
-        secondHandView h =
-            S.rect
-                [ SA.x "59"
-                , SA.y "59"
-                , SA.width "2"
-                , SA.height "60"
-                , SA.rx "1"
-                , SA.ry "1"
-                , SA.stroke "red"
-                , SA.fill "red"
-                , SA.transform ("rotate(" ++ String.fromInt (handDegree 60 h) ++ " 60 60)")
-                ]
-                []
-
-        markView h =
-            S.rect
-                [ SA.x "59"
-                , SA.y "4"
-                , SA.width "2"
-                , SA.height "20"
-                , SA.rx "1"
-                , SA.ry "1"
-                , SA.stroke "black"
-                , SA.fill "black"
-                , SA.strokeOpacity "90%"
-                , SA.fillOpacity "90%"
-                , SA.transform ("rotate(" ++ String.fromInt (handDegree 12 h) ++ " 60 60)")
-                ]
-                []
-
-        markViews =
-            List.map markView (List.range 0 12)
+        -- expirationView : Expiration -> Model -> Html Msg
+        -- expirationView expiration =
+        --     p [] [ text expirationText (timeLeft model.time expiration)]
     in
     div []
-        [ h1 [] [ text (hourView ++ ":" ++ minuteView ++ ":" ++ secondView) ]
-        , if model.paused then
-            button [ onClick Resume ] [ text "Resume" ]
+        [ case model.connection of
+            Connected expiration ->
+                div []
+                    [ p []
+                        [ text ("Connected " ++ expirationText (timeLeft model.time expiration))
+                        ]
+                    , button
+                        [ onClick StartDisconnect ]
+                        [ text "Disconnect" ]
+                    ]
 
-          else
-            button [ onClick Pause ] [ text "Pause" ]
-        , S.svg
-            [ SA.width "120"
-            , SA.height "120"
-            , SA.viewBox "0 0 120 120"
-            ]
-            (List.append
-                markViews
-                [ hourHandView hour
-                , minuteHandView minute
-                , secondHandView second
-                ]
-            )
+            Disconnected expiration ->
+                div []
+                    [ p []
+                        [ text ("Disconnected " ++ expirationText (timeLeft model.time expiration))
+                        ]
+                    , button
+                        [ onClick StartConnect ]
+                        [ text "Connect" ]
+                    ]
         ]
