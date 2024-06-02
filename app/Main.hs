d9064648744c8fd7b141ad64fcba6df1b69bec86
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2024-06-02 20:57:20 +0000

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,9 @@ import Data.ByteString.Char8 as C
 import Happstack.Server (dir, nullConf, nullDir, ok, seeOther, simpleHTTP, toResponse, toResponseBS)
 import Lucid
 import Lucid.Htmx
-import System.Random
+import System.Random ( initStdGen, uniformR, RandomGen )
+import Data.Time.Clock (UTCTime, addUTCTime, getCurrentTime)
+import Control.Monad.IO.Class (MonadIO(liftIO))
 
 numbersTemplate :: Int -> Html ()
 numbersTemplate n =
@@ -17,8 +19,35 @@ numbersTemplate n =
       ( mapM_ (li_ . toHtml . show) [1 .. n]
       )
 
+data Expiration = Expired UTCTime | NotExpired UTCTime
+
+connectedView :: Expiration -> Html ()
+connectedView (Expired _) =
+  p_ "Connected"
+  <> p_ "Expired already"
+connectedView (NotExpired _) =
+  p_ "Connected"
+  <> p_ "Expiring in xxx"
+  <> button_ [
+    hxPost_ "/disconnect"
+  ] "Disconnect" 
+
+disconnectedView :: Expiration -> Html ()
+disconnectedView (Expired _) =
+  p_ "Disconnected"
+  <> p_ "Expired already"
+disconnectedView (NotExpired _) =
+  p_ "Disconnected"
+  <> p_ "Expiring in xxx"
+  <> button_ [
+    hxPost_ "/connect"
+  ] "Connect" 
+
 generateDelay :: RandomGen g => g -> Int
 generateDelay = fst . uniformR (3,10)
+
+calcExpiration ::  Integer -> UTCTime -> UTCTime
+calcExpiration = addUTCTime . fromInteger
 
 doc :: () -> Html ()
 doc _ =
@@ -32,7 +61,7 @@ doc _ =
             <> script_ [src_ "https://unpkg.com/htmx.org@1.9.12"] ("" :: Html ())
         )
         <> body_
-          [ hxGet_ "/body",
+          [ hxPost_ "/connect",
             hxTrigger_ "load delay:1s",
             hxSwap_ "innerHTML"
           ]
@@ -45,8 +74,11 @@ main =
     msum
       [ mzero,
         do {
-          g <- initStdGen
-          ; dir "body" $ ok $ toResponseBS (C.pack "text/html;charset=utf-8") $ renderBS $ numbersTemplate $ generateDelay g -- mkStdGen 137
+          t <- liftIO getCurrentTime
+          ; g <- initStdGen
+          ; let delay = toInteger $ generateDelay g
+          ; let expiration = calcExpiration delay t
+          ; dir "connect" $ ok $ toResponseBS (C.pack "text/html;charset=utf-8") $ renderBS $ connectedView (NotExpired expiration)
         },
         nullDir >> ok (toResponseBS (C.pack "text/html;charset=utf-8") $ renderBS $ doc ()),
         seeOther ("/" :: String) $ toResponse ("Goto to root" :: String)
